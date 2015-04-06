86e6bc60449b57caea0261d07517af13fa77e515
rayxke <rayxke@users.noreply.github.com>
2015-04-06 10:59:56 -0400

    Update mult_lpm.vhd
    Changed inputs to 16 bit and out to 32 bit

--- Project_Code/mult_lpm.vhd
+++ Project_Code/mult_lpm.vhd
@@ -42,16 +42,16 @@ USE lpm.all;
 ENTITY mult_lpm IS
 	PORT
 	(
-		dataa		: IN STD_LOGIC_VECTOR (7 DOWNTO 0);
-		datab		: IN STD_LOGIC_VECTOR (7 DOWNTO 0);
-		result		: OUT STD_LOGIC_VECTOR (15 DOWNTO 0)
+		dataa		: IN STD_LOGIC_VECTOR (15 DOWNTO 0);
+		datab		: IN STD_LOGIC_VECTOR (15 DOWNTO 0);
+		result		: OUT STD_LOGIC_VECTOR (31 DOWNTO 0)
 	);
 END mult_lpm;
 
 
 ARCHITECTURE SYN OF mult_lpm IS
 
-	SIGNAL sub_wire0	: STD_LOGIC_VECTOR (15 DOWNTO 0);
+	SIGNAL sub_wire0	: STD_LOGIC_VECTOR (31 DOWNTO 0);
 
 
 
@@ -65,23 +65,23 @@ ARCHITECTURE SYN OF mult_lpm IS
 		lpm_widthp		: NATURAL
 	);
 	PORT (
-			dataa	: IN STD_LOGIC_VECTOR (7 DOWNTO 0);
-			datab	: IN STD_LOGIC_VECTOR (7 DOWNTO 0);
-			result	: OUT STD_LOGIC_VECTOR (15 DOWNTO 0)
+			dataa	: IN STD_LOGIC_VECTOR (15 DOWNTO 0);
+			datab	: IN STD_LOGIC_VECTOR (15 DOWNTO 0);
+			result	: OUT STD_LOGIC_VECTOR (31 DOWNTO 0)
 	);
 	END COMPONENT;
 
 BEGIN
-	result    <= sub_wire0(15 DOWNTO 0);
+	result    <= sub_wire0(31 DOWNTO 0);
 
 	lpm_mult_component : lpm_mult
 	GENERIC MAP (
 		lpm_hint => "MAXIMIZE_SPEED=5",
-		lpm_representation => "UNSIGNED",
+		lpm_representation => "SIGNED",
 		lpm_type => "LPM_MULT",
-		lpm_widtha => 8,
-		lpm_widthb => 8,
-		lpm_widthp => 16
+		lpm_widtha => 16,
+		lpm_widthb => 16,
+		lpm_widthp => 32
 	)
 	PORT MAP (
 		dataa => dataa,
@@ -103,32 +103,32 @@ END SYN;
 -- Retrieval info: PRIVATE: LPM_PIPELINE NUMERIC "0"
 -- Retrieval info: PRIVATE: Latency NUMERIC "0"
 -- Retrieval info: PRIVATE: SYNTH_WRAPPER_GEN_POSTFIX STRING "0"
--- Retrieval info: PRIVATE: SignedMult NUMERIC "0"
+-- Retrieval info: PRIVATE: SignedMult NUMERIC "1"
 -- Retrieval info: PRIVATE: USE_MULT NUMERIC "1"
 -- Retrieval info: PRIVATE: ValidConstant NUMERIC "0"
--- Retrieval info: PRIVATE: WidthA NUMERIC "8"
--- Retrieval info: PRIVATE: WidthB NUMERIC "8"
--- Retrieval info: PRIVATE: WidthP NUMERIC "16"
+-- Retrieval info: PRIVATE: WidthA NUMERIC "16"
+-- Retrieval info: PRIVATE: WidthB NUMERIC "16"
+-- Retrieval info: PRIVATE: WidthP NUMERIC "32"
 -- Retrieval info: PRIVATE: aclr NUMERIC "0"
 -- Retrieval info: PRIVATE: clken NUMERIC "0"
 -- Retrieval info: PRIVATE: optimize NUMERIC "0"
 -- Retrieval info: CONSTANT: LPM_HINT STRING "MAXIMIZE_SPEED=5"
--- Retrieval info: CONSTANT: LPM_REPRESENTATION STRING "UNSIGNED"
+-- Retrieval info: CONSTANT: LPM_REPRESENTATION STRING "SIGNED"
 -- Retrieval info: CONSTANT: LPM_TYPE STRING "LPM_MULT"
--- Retrieval info: CONSTANT: LPM_WIDTHA NUMERIC "8"
--- Retrieval info: CONSTANT: LPM_WIDTHB NUMERIC "8"
--- Retrieval info: CONSTANT: LPM_WIDTHP NUMERIC "16"
--- Retrieval info: USED_PORT: dataa 0 0 8 0 INPUT NODEFVAL dataa[7..0]
--- Retrieval info: USED_PORT: datab 0 0 8 0 INPUT NODEFVAL datab[7..0]
--- Retrieval info: USED_PORT: result 0 0 16 0 OUTPUT NODEFVAL result[15..0]
--- Retrieval info: CONNECT: @dataa 0 0 8 0 dataa 0 0 8 0
--- Retrieval info: CONNECT: result 0 0 16 0 @result 0 0 16 0
--- Retrieval info: CONNECT: @datab 0 0 8 0 datab 0 0 8 0
+-- Retrieval info: CONSTANT: LPM_WIDTHA NUMERIC "16"
+-- Retrieval info: CONSTANT: LPM_WIDTHB NUMERIC "16"
+-- Retrieval info: CONSTANT: LPM_WIDTHP NUMERIC "32"
+-- Retrieval info: USED_PORT: dataa 0 0 16 0 INPUT NODEFVAL dataa[15..0]
+-- Retrieval info: USED_PORT: datab 0 0 16 0 INPUT NODEFVAL datab[15..0]
+-- Retrieval info: USED_PORT: result 0 0 32 0 OUTPUT NODEFVAL result[31..0]
+-- Retrieval info: CONNECT: @dataa 0 0 16 0 dataa 0 0 16 0
+-- Retrieval info: CONNECT: result 0 0 32 0 @result 0 0 32 0
+-- Retrieval info: CONNECT: @datab 0 0 16 0 datab 0 0 16 0
 -- Retrieval info: LIBRARY: lpm lpm.lpm_components.all
 -- Retrieval info: GEN_FILE: TYPE_NORMAL mult_lpm.vhd TRUE
 -- Retrieval info: GEN_FILE: TYPE_NORMAL mult_lpm.inc FALSE
 -- Retrieval info: GEN_FILE: TYPE_NORMAL mult_lpm.cmp TRUE
--- Retrieval info: GEN_FILE: TYPE_NORMAL mult_lpm.bsf TRUE
+-- Retrieval info: GEN_FILE: TYPE_NORMAL mult_lpm.bsf FALSE
 -- Retrieval info: GEN_FILE: TYPE_NORMAL mult_lpm_inst.vhd FALSE
 -- Retrieval info: GEN_FILE: TYPE_NORMAL mult_lpm_waveforms.html TRUE
 -- Retrieval info: GEN_FILE: TYPE_NORMAL mult_lpm_wave*.jpg FALSE
